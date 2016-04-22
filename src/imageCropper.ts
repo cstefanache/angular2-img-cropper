7eb8956f554436638471d95427c63867d267f431
cstefanache <cornel.stefanache@gmail.com>
2016-04-22 20:18:52 +0300

    removed browser typing dependency

--- src/imageCropper.ts
+++ src/imageCropper.ts
@@ -1,6 +1,3 @@
-///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
-
-
 import {Component, Renderer, ViewChild, ElementRef} from 'angular2/core';
 import {PointPool} from './model/pointPool';
 import {Point} from './model/point';
