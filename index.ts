961880c8a3b1ddf11f975624a5a7b64305fe701a
Jurien <jurien@kings-dev.io>
2016-09-23 11:39:21 +0200

    added few extra exports

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
+// core
 export { ImageCropperModule } from './src/imageCropperModule';
-export { CropperSettings } from './src/cropperSettings';
 export { ImageCropperComponent } from './src/imageCropperComponent';
+
+// extra classes
+export { ImageCropper } from './src/imageCropper';
+export { CropperSettings } from './src/cropperSettings';
+export { CropperDrawSettings } from './src/cropperDrawSettings';
+
+// models
+export { Bounds } from './src/model/bounds';
