f328b5c1bf0398b45026cb834fe67e63b79e6375
Enrico Sorio <enrico.sorio@gmail.com>
2016-09-26 00:01:25 +0200

    removed ng-styler

--- runtime/app.ts
+++ runtime/app.ts
@@ -1,105 +1,43 @@
 import {Component, ViewChild, Type} from '@angular/core';
 import {ImageCropperComponent, CropperSettings, Bounds} from '../index';
-import {Style} from 'ng2-styler';
 
-
-@Style({
-    "h3": {
-      paddingBottom: "20px",
-      borderBottom: "1px solid #A0A0A0"
-    },
-
-    "h4": {
-        paddingBottom: "20px",
-        paddingTop: "20px",
-        borderTop: "1px solid #A0A0A0"
-    },
-    ".result": {
-        marginTop: "30px",
-        border: "1px solid rgba(125,125,125,0.6)",
-        display: "inline-block",
-        padding: "1px",
-
-        "&.rounded": {
-            "> img": {
-                borderRadius: "100px"
-            }
-        }
-    },
-
-    ".pull-left": {
-        minWidth: "400px",
-        float: "left",
-        marginRight: "10px",
-        padding: "10px",
-        backgroundColor: "rgba(0,0,0,0.05)",
-        position: "relative",
-
-    },
-
-
-    ".file-upload": {
-        height: "25px",
-        width: "100px",
-        position: "relative",
-        borderRadius: "3px",
-
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-
-        border: "1px solid #FFFFFF",
-        overflow: "hidden",
-        backgroundImage: "linear-gradient(to bottom, #2590EB 50%, #FFFFFF 50%)",
-        backgroundSize: "100% 200%",
-        transition: "all 1s",
-        color: "#FFFFFF",
-        fontSize: "100px",
-
-        "&:hover": {
-            backgroundPosition: "0 -100%",
-            color: "#2590EB"
-        },
-
-        ".text": {
-            fontSize: "14px"
-        },
-
-
-        "input[type='file']": {
-            height: "25px",
-            width: "100px",
-            position: "absolute",
-            top: "0",
-            left: "0",
-            opacity: "0",
-            cursor: "pointer"
-        }
-
-    }
-})
 @Component({
     selector: 'test-app',
     template: `
-    <div>
-        <div class="pull-left">
-            <h3>Sample cropper 1</h3>
+    <div class="row">
+      <h2>Sample cropper 1</h2>
+      <div class="col s6">
+        <div class="card">
+          <div class="card-image">
             <img-cropper [image]="data1" [settings]="cropperSettings1" (onCrop)="cropped($event)"></img-cropper>
-            <br>
-            <span class="result" *ngIf="data1.image" >
-                <img [src]="data1.image" [width]="cropperSettings1.croppedWidth" [height]="cropperSettings1.croppedHeight">
-            </span>
-            <h4>Settings:</h4>
-            <pre>
-this.cropperSettings1 = new CropperSettings();
+            <span class="card-title">source image</span>
+          </div>
+          <div class="card-content">
+          </div>
+        </div>
+      </div>
+
+
+    <div class="col s6">
+      <div class="card horizontal">
+        <div class="card-image">
+        <span class="result" *ngIf="data1.image" >
+            <img [src]="data1.image" [width]="cropperSettings1.croppedWidth" [height]="cropperSettings1.croppedHeight">
+        </span>
+        </div>
+        <div class="card-stacked">
+          <div class="card-content">
+          <span class="card-title">settings</span>
+<div>
+<pre><code>this.cropperSettings1 = new CropperSettings();
 this.cropperSettings1.width = 200;
 this.cropperSettings1.height = 200;
 
 this.cropperSettings1.croppedWidth = 200;
 this.cropperSettings1.croppedHeight = 200;
 
-this.cropperSettings1.canvasWidth = 500;
-this.cropperSettings1.canvasHeight = 300;
+this.cropperSettings1.canvasWidth = 691;
+this.cropperSettings1.canvasHeight = 377;
 
 this.cropperSettings1.minWidth = 100;
 this.cropperSettings1.minHeight = 100;
@@ -108,8 +46,14 @@ this.cropperSettings1.rounded = false;
 
 this.cropperSettings1.cropperDrawSettings.strokeColor = 'rgba(255,255,255,1)';
 this.cropperSettings1.cropperDrawSettings.strokeWidth = 2;
-            </pre>
+</code></pre>
+            </div>
+          </div>
         </div>
+      </div>
+    </div>
+
+  </div>
 
 
         <div class="pull-left">
@@ -150,8 +94,7 @@ this.cropperSettings2.cropperDrawSettings.strokeWidth = 2;
 this.cropperSettings2.noFileInput = true;
             </pre>
         </div>
-    </div>`,
-    directives: [ImageCropperComponent]
+    `
 })
 export class AppComponent extends Type {
 
@@ -170,13 +113,13 @@ export class AppComponent extends Type {
 
         this.cropperSettings1 = new CropperSettings();
         this.cropperSettings1.width = 200;
-        this.cropperSettings1.height = 200;
+        this.cropperSettings1.height = 300;
 
         this.cropperSettings1.croppedWidth = 200;
-        this.cropperSettings1.croppedHeight = 200;
+        this.cropperSettings1.croppedHeight = 300;
 
-        this.cropperSettings1.canvasWidth = 500;
-        this.cropperSettings1.canvasHeight = 300;
+        this.cropperSettings1.canvasWidth = 691;
+        this.cropperSettings1.canvasHeight = 377;
 
         this.cropperSettings1.minWidth = 100;
         this.cropperSettings1.minHeight = 100;
@@ -185,6 +128,13 @@ export class AppComponent extends Type {
 
         this.cropperSettings1.cropperDrawSettings.strokeColor = 'rgba(255,255,255,1)';
         this.cropperSettings1.cropperDrawSettings.strokeWidth = 2;
+
+        this.cropperSettings1.keepAspect = true;
+
+        this.cropperSettings1.initialX = 305;
+        this.cropperSettings1.initialY = 60;
+        this.cropperSettings1.initialW = 119;
+        this.cropperSettings1.initialH = 119;
 
         this.data1 = {};
 
