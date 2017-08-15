34d862cd2d489b856d203e1e9ef8bfba7efbe023
Mounir R'Quiba <mounir.rquiba@gmail.com>
2017-08-15 14:30:59 +0200

    Add default strokeColor

--- src/model/cornerMarker.ts
+++ src/model/cornerMarker.ts
@@ -54,7 +54,7 @@ export class CornerMarker extends Handle implements ICornerMarker {
         ctx.lineTo(this.position.x + this.offset.x, this.position.y + this.offset.y);
         ctx.closePath();
         ctx.lineWidth = this.cropperSettings.cropperDrawSettings.strokeWidth;
-        ctx.strokeStyle = this.cropperSettings.cropperDrawSettings.strokeColor;
+        ctx.strokeStyle = this.cropperSettings.cropperDrawSettings.strokeColor || 'rgba(255,255,255,.7)';
         ctx.stroke();
     }
 
