17f939e85ee7f8585db34626d18eb9570e45062e
ys01se <ys01se@gmail.com>
2017-09-22 13:18:05 +0100

    Add a new variable to control the touch radius of the center drag marker (independently of the corner markers) to improve control using a mobile device

--- src/cropperSettings.ts
+++ src/cropperSettings.ts
@@ -47,6 +47,7 @@ export class CropperSettings implements ICropperSettings {
 
     public resampleFn:Function;
 
+    public centerTouchRadius: number = 20;
     public showCenterMarker:boolean = true;
 
     public allowedFilesRegex: RegExp = /\.(jpe?g|png|gif|bmp)$/i;
